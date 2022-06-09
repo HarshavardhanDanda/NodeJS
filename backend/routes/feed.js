c4353136bac4c1dad1bc0a4550ee713b568a9764
Harshazemoso <harshavardhan.danda@zemosolabs.com>
2022-06-09 10:30:58 +0530

    first commit

---
+++ backend/routes/feed.js
@@ -0,0 +1,22 @@
+const { Router } = require('express');
+const express = require('express');
+
+
+const feedController = require('../controllers/feed');
+const isAuth = require('../middleware/is-auth')
+
+const router = express.Router();
+
+// GET /feed/posts
+router.get('/posts', isAuth, feedController.getPosts);
+
+// POST /feed/post
+router.post('/post', isAuth, feedController.createPost);
+
+router.get('/post/:postId', isAuth, feedController.getPost)
+
+router.put('/post/:postId', isAuth, feedController.updatePost)
+
+router.delete('/post/:postId', isAuth, feedController.deletePost)
+
+module.exports = router;
